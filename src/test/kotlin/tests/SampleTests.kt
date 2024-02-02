5b8c8c45473487077a23b5bde06384937abebcbb
azzumwaqar <azzumwaqar@gmail.com>
2024-02-02 10:19:19 +0000

    Tutorial 5 - json Path finder

--- src/test/kotlin/tests/SampleTests.kt
+++ src/test/kotlin/tests/SampleTests.kt
@@ -2,26 +2,38 @@ package tests
 
 import io.restassured.RestAssured.*
 import org.hamcrest.CoreMatchers.*
+import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
 
 class SampleTests {
 
     companion object {
         private const val GET_BOOKINGS = "https://restful-booker.herokuapp.com/booking"
-        private const val GET_A_BOOKING = GET_BOOKINGS
+    }
+
+    private  var id:Int? = null
+
+    @BeforeMethod
+    fun setUp() {
+        baseURI = GET_BOOKINGS
     }
 
     @Test
     fun get_booking_ids() {
-        get(GET_BOOKINGS).then().statusCode(200).body(notNullValue())
+        get().then().statusCode(200).body(notNullValue())
+
     }
 
 
-
     @Test
-    fun get_a_booking_with_id(){
-         get("${GET_BOOKINGS}/1000").then().statusCode(200)
+    fun get_a_booking_with_id() {
+        id = get().body.path("[0].bookingid")
 
+        given()
+            .get("/$id")
+            .then()
+            .statusCode(200)
+            .body("firstname", `is`("Josh"))
     }
 
 
